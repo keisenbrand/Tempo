438cdbe4df78495c5923ef93f4541472381ed630
Katherine Eisenbrand <keisenbrand@gmail.com>
2018-03-08 16:21:02 -0500

    Collection View layout WIP

--- Tempo/Tempo/Views/BrandCell.swift
+++ Tempo/Tempo/Views/BrandCell.swift
@@ -12,5 +12,18 @@ class BrandCell: UICollectionViewCell {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var overlayView: UIView!
     
+    override var isSelected: Bool {
+        didSet {
+            if self.isSelected {
+                self.overlayView.alpha = 0.35
+                overlayView.layer.borderWidth = 3
+                overlayView.layer.borderColor = UIColor(red:0.41, green:0.62, blue:0.93, alpha:1.0).cgColor
+            } else {
+                overlayView.alpha = 0.3
+                overlayView.layer.borderWidth = 0
+            }
+        }
+    }
 }
